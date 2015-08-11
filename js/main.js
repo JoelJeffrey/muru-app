773992f64826468e974c15cac0c17db76210d75a
Joel Jeffrey <Joel@nine15.com>
2015-08-10 20:27:28 -0500

    added jquery swipe plugin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function($) {
 	
-	
+	$(function() {      
+		$(".page").swipe( {
+			//Generic swipe handler for all directions
+			swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+				console.log("You swiped " + direction );  
+			}
+		});
+	});
 
 });
